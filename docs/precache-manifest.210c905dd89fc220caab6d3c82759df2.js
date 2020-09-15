f993ac0d34fe1aa2192755f6b83df8b4633ac3c1
Carlos Emmanuel Arcos Novelo <calinchat@hotmail.com>
2020-09-15 18:29:13 -0500

    Docs agregado

---
+++ docs/precache-manifest.210c905dd89fc220caab6d3c82759df2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef0f91c870369fab9993af73da80110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4da6e1ee320a8863eaf",
+    "url": "/static/css/main.080b476c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4da6e1ee320a8863eaf",
+    "url": "/static/js/main.b44d89e2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
